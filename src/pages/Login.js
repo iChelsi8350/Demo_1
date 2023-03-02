9ed6c4596e971e8867daa3bc61bb02cac401c188
Chelsi <chelsi.divtech@gmail.com>
2023-03-02 19:14:34 +0530

    add validations

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 const Login = () => {
   const navigate = useNavigate();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    navigate("/signup");
-  };
+  const ValidationSchema = Yup.object().shape({
+    email: Yup.string().email().required("Email Required"),
+    password: Yup.string()
+      .required("Password Required")
+      .min(8, "Password is Too short")
+      .matches(/(?=.*[0-9])/, "Must Contain Number"),
+  });
+
+  //   const handleSubmit = (event) => {
+  //     event.preventDefault();
+  //     navigate("/signup");
+  //   };
 
   return (
     <>
@@ -16,41 +26,79 @@ const Login = () => {
       <img className="img4" alt="" src="/images/Group 1000002089.png" />
       <div className="login-form-container">
         <div className="login-form">
-          <form onSubmit={handleSubmit}>
-            <h4 className="modal-title">Welcome back, Ouly.AI</h4>
-            <p>Welcome back! please enter your details.</p>
-            <div className="form-group">
-              <label className="form-label">Email</label>
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Ouly12@gmail.com"
-                required="required"
-              />
-            </div>
-            <div className="form-group">
-              <label className="form-label">Password</label>
-              <input
-                type="password"
-                className="form-control"
-                placeholder="..........."
-                required="required"
-              />
-            </div>
-            <div className="form-group small clearfix">
-              <label className="checkbox-inline">
-                <input type="checkbox" /> Remember me
-              </label>
-              <Link to="#" className="forgot-link">
-                Forgot Password?
-              </Link>
-            </div>
-            <div className="d-grid gap-2">
-              <button className="btn btn-primary text-white" type="submit">
-                Log in
-              </button>
-            </div>
-          </form>
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            onSubmit={(values) => {
+              console.log(values);
+              navigate("/signup");
+            }}
+            validationSchema={ValidationSchema}
+          >
+            {({
+              values,
+              errors,
+              handleSubmit,
+              handleChange,
+              handleBlur,
+              touched,
+            }) => {
+              return (
+                <form onSubmit={handleSubmit}>
+                  <h4 className="modal-title">Welcome back, Ouly.AI</h4>
+                  <p>Welcome back! please enter your details.</p>
+                  <div className="form-group">
+                    <label className="form-label">Email</label>
+                    <input
+                      name="email"
+                      type="email"
+                      className="form-control"
+                      placeholder="ouly12@gmail.com"
+                      value={values.email}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+
+                    {errors.email && touched.email && (
+                      <div className="input feedback error">{errors.email}</div>
+                    )}
+                  </div>
+                  <div className="form-group">
+                    <label className="form-label">Password</label>
+                    <input
+                      name="password"
+                      type="password"
+                      className="form-control"
+                      placeholder="..........."
+                      value={values.password}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    {errors.password && touched.password && (
+                      <div className="input feedback error">
+                        {errors.password}
+                      </div>
+                    )}
+                  </div>
+                  <div className="form-group small clearfix">
+                    <label className="checkbox-inline">
+                      <input type="checkbox" /> Remember me
+                    </label>
+                    <Link to="#" className="forgot-link">
+                      Forgot Password?
+                    </Link>
+                  </div>
+                  <div className="d-grid gap-2">
+                    <button
+                      className="btn btn-primary text-white"
+                      type="submit"
+                    >
+                      Log in
+                    </button>
+                  </div>
+                </form>
+              );
+            }}
+          </Formik>
           <div className="text-center small">
             Don't have an account? <Link to="/signup">Sign up</Link>
           </div>
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,13 +1,26 @@
+import { Formik } from "formik";
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import * as Yup from "yup";
 
 const SignUp = () => {
   const navigate = useNavigate();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    navigate("/");
-  };
+  const ValidationSchema = Yup.object().shape({
+    firstname: Yup.string().required("First Name is Required"),
+    lastname: Yup.string().required("Last Name is Required"),
+    email: Yup.string().email().required("Email Required"),
+    mobileno: Yup.number().required("Mobile Number is Required"),
+    password: Yup.string()
+      .required("Password Required")
+      .min(8, "Password is Too short")
+      .matches(/(?=.*[0-9])/, "Must Contain Number"),
+  });
+
+  //   const handleSubmit = (event) => {
+  //     event.preventDefault();
+  //     navigate("/");
+  //   };
 
   return (
     <>
@@ -17,66 +30,124 @@ const SignUp = () => {
       </div>
 
       <div className="login-form">
-        <form onSubmit={handleSubmit}>
-          <h4 className="modal-title">Welcome back, Ouly.AI</h4>
-          <p>Welcome! please enter your details.</p>
-          <div className="row">
-            <div className="form-group col-md-6">
-              <label className="form-label">First Name</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter First Name"
-                required="required"
-              />
-            </div>
-            <div className="form-group col-md-6">
-              <label className="form-label">Last Name</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="EnterLast Name"
-                required="required"
-              />
-            </div>
-          </div>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          onSubmit={(values) => {
+            console.log(values);
+            navigate("/");
+          }}
+          validationSchema={ValidationSchema}
+        >
+          {({
+            values,
+            errors,
+            handleSubmit,
+            handleChange,
+            handleBlur,
+            touched,
+          }) => {
+            return (
+              <form onSubmit={handleSubmit}>
+                <h4 className="modal-title">Welcome back, Ouly.AI</h4>
+                <p>Welcome! please enter your details.</p>
+                <div className="row">
+                  <div className="form-group col-md-6">
+                    <label className="form-label">First Name</label>
+                    <input
+                      name="firstname"
+                      type="text"
+                      className="form-control"
+                      placeholder="Enter First Name"
+                      value={values.firstname}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    {errors.firstname && touched.firstname && (
+                      <div className="input feedback error">
+                        {errors.firstname}
+                      </div>
+                    )}
+                  </div>
+                  <div className="form-group col-md-6">
+                    <label className="form-label">Last Name</label>
+                    <input
+                      name="lastname"
+                      type="text"
+                      className="form-control"
+                      placeholder="EnterLast Name"
+                      value={values.lastname}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    {errors.lastname && touched.lastname && (
+                      <div className="input feedback error">
+                        {errors.lastname}
+                      </div>
+                    )}
+                  </div>
+                </div>
 
-          <div className="form-group">
-            <label className="form-label">Email</label>
-            <input
-              type="email"
-              className="form-control"
-              placeholder="Ouly12@gmail.com"
-              required="required"
-            />
-          </div>
+                <div className="form-group">
+                  <label className="form-label">Email</label>
+                  <input
+                    name="email"
+                    type="email"
+                    className="form-control"
+                    placeholder="Ouly12@gmail.com"
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {errors.email && touched.email && (
+                    <div className="input feedback error">{errors.email}</div>
+                  )}
+                </div>
 
-          <div className="form-group">
-            <label className="form-label">Mobile No</label>
-            <input
-              type="number"
-              className="form-control"
-              placeholder="Enter Mobile No"
-              required="required"
-            />
-          </div>
+                <div className="form-group">
+                  <label className="form-label">Mobile No</label>
+                  <input
+                    name="mobileno"
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter Mobile No"
+                    value={values.mobileno}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {errors.mobileno && touched.mobileno && (
+                    <div className="input feedback error">
+                      {errors.mobileno}
+                    </div>
+                  )}
+                </div>
 
-          <div className="form-group">
-            <label className="form-label">Password</label>
-            <input
-              type="password"
-              className="form-control"
-              placeholder=".........."
-              required="required"
-            />
-          </div>
+                <div className="form-group">
+                  <label className="form-label">Password</label>
+                  <input
+                    name="password"
+                    type="password"
+                    className="form-control"
+                    placeholder="..........."
+                    value={values.password}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {errors.password && touched.password && (
+                    <div className="input feedback error">
+                      {errors.password}
+                    </div>
+                  )}
+                </div>
 
-          <div className="d-grid gap-2">
-            <button className="btn btn-primary text-white" type="submit">
-              Sign up
-            </button>
-          </div>
-        </form>
+                <div className="d-grid gap-2">
+                  <button className="btn btn-primary text-white" type="submit">
+                    Sign up
+                  </button>
+                </div>
+              </form>
+            );
+          }}
+        </Formik>
         <div className="text-center small">
           Already have an account? <Link to="/">Sign in</Link>
         </div>
